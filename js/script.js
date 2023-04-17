54ee06e26e021ab95b9a1e734bc4e01c84f4f03c
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-04-17 16:31:53 +0200

    feat: add FizzBuzz in DOM

--- js/script.js
+++ js/script.js
@@ -2,24 +2,31 @@ const contenitore = document.querySelector(".container")
 const casella = document.querySelector(".box")
 
 
-for (let i = 0; i <= 100 ; i++) {
+for (let i = 1; i <= 100 ; i++) {
 
     let buzz = "Buzz"
     let fizzBuzz = "FizzBuzz"
     let fizz = "Fizz"
     let numero = i
 
+    
+    
+    if (i % 3 == 0 && i % 5 == 0)
+    //console.log (fizzBuzz)
+    contenitore.innerHTML += `<div class="box">${fizzBuzz}</div>`
+    else if (i % 3 == 0)
+    //console.log (fizz)
+    contenitore.innerHTML += `<div class="box">${fizz}</div>`
+    else if ( i % 5 == 0)
+    //console.log (buzz)
+    contenitore.innerHTML += `<div class="box">${buzz}</div>`
+    else
+    //console.log (i)
     contenitore.innerHTML += `<div class="box">${numero}</div>`
+    
+}
     
    
 
-    if (i % 3 == 0 && i % 5 == 0)
-    console.log (fizzBuzz)
-    else if (i % 3 == 0)
-    console.log (fizz)
-    else if ( i % 5 == 0)
-    console.log (buzz)
-    else
-    console.log (i)
     
-}
+   
