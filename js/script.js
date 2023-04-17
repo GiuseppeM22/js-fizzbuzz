a78602979272ff8037e365554a369d57d94113f0
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-04-17 16:57:15 +0200

    feat: add box color

--- js/script.js
+++ js/script.js
@@ -4,22 +4,22 @@ const casella = document.querySelector(".box")
 
 for (let i = 1; i <= 100 ; i++) {
 
-    let buzz = "Buzz"
+    let buzz = "Buzz" 
     let fizzBuzz = "FizzBuzz"
     let fizz = "Fizz"
     let numero = i
 
     
     
-    if (i % 3 == 0 && i % 5 == 0)
+    if (i % 3 == 0 && i % 5 == 0 )
     //console.log (fizzBuzz)
-    contenitore.innerHTML += `<div class="box">${fizzBuzz}</div>`
+    contenitore.innerHTML += `<div class="box red">${fizzBuzz}</div>`
     else if (i % 3 == 0)
     //console.log (fizz)
-    contenitore.innerHTML += `<div class="box">${fizz}</div>`
+    contenitore.innerHTML += `<div class="box blue">${fizz}</div>`
     else if ( i % 5 == 0)
     //console.log (buzz)
-    contenitore.innerHTML += `<div class="box">${buzz}</div>`
+    contenitore.innerHTML += `<div class="box gold">${buzz}</div>`
     else
     //console.log (i)
     contenitore.innerHTML += `<div class="box">${numero}</div>`
